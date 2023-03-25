bd2dd93b3bd21346edbb4e9b3233b550a4fa01ba
Julia Sałacka <salacka.julia@gmail.com>
2023-03-25 19:18:05 +0100

    ustawienie kolejności warstw + trigger

---
+++ Assets/Scripts/Environment_scripts/Obj_change_layer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Obj_change_layer : MonoBehaviour
+{
+    public Transform stairs; // The object whose Sorting Group will be changed
+
+    private UnityEngine.Rendering.SortingGroup sortingGroup; // Reference to the Sorting Group component
+
+    void Start()
+    {
+        sortingGroup = stairs.GetComponent<SortingGroup>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        sortingGroup.sortingOrder = 0;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        sortingGroup.sortingOrder = 1;
+    }
+}
